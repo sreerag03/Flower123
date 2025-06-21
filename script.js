0f928d28bc8ed48aeb785d8932d8506c22bccacc
Sreerag Premanathan <42859992+sreerag03@users.noreply.github.com>
2025-06-21 15:48:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('bloomBtn').addEventListener('click', function() {
+  const bouquet = document.getElementById('bouquet');
+  bouquet.classList.remove('hidden');
+  bouquet.classList.add('show');
+});
